86114509a93c672c0506f59a5f63c4d778f840f7
Pablo Molina <contact@pablomolina.me>
2020-07-07 09:31:37 +0200

    Parse links

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -1,3 +1,32 @@
+const parseLinks = (doc, { fromLine, toLine }) => {
+  doc.eachLine(fromLine, toLine, (line) => {
+    const lineNumber = doc.getLineNumber(line);
+
+    line.text.replace(/https?:\/\/[^\s]+/gi, (url, index) => {
+      const linkElement = document.createElement('a');
+
+      linkElement.classList.add('editor-inline-link');
+      linkElement.href = url;
+      linkElement.innerText = url
+        .replace(/^https:\/\/issues\.liferay\.com\/browse\//i, '')
+        .replace(/^https:\/\/github\.com\//i, '')
+        .replace(/^https?:\/\//i, '');
+
+      doc.markText(
+        { line: lineNumber, ch: index },
+        { line: lineNumber, ch: index + url.length },
+        {
+          replacedWith: linkElement,
+          inclusiveLeft: false,
+          inclusiveRight: false,
+        }
+      );
+
+      return url;
+    });
+  });
+};
+
 window.createEditor = async () => {
   const editor = CodeMirror(document.getElementById('editor'), {
     value: await StorageService.getValue(),
@@ -6,11 +35,19 @@ window.createEditor = async () => {
     lineWrapping: true,
   });
 
+  const doc = editor.getDoc();
+  parseLinks(doc, { fromLine: 0, toLine: doc.lastLine() + 1 });
+
   StorageService.onChange((text) => {
-    editor.getDoc().setValue(text);
+    doc.setValue(text);
   });
 
-  editor.on('change', () => {
-    StorageService.setValue(editor.getDoc().getValue());
+  editor.on('change', (_, range) => {
+    parseLinks(doc, {
+      fromLine: range.from.line - 1,
+      toLine: range.to.line + 1,
+    });
+
+    StorageService.setValue(doc.getValue());
   });
 };
