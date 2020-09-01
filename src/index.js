3d970b3b56a394ea4fee868f2f868f70e658266d
Pablo Molina <contact@pablomolina.me>
2020-09-01 13:25:57 +0200

    Add hi animation

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import { I18NService } from './services/I18NService.js';
 document.head.querySelector('title').innerText = I18NService.get('newTab');
 
 StorageService.init().then(() => {
-  preact.render(
-    html`<${CSSContextProvider}><${App} /><//>`,
-    document.getElementById('app')
-  );
+  const app = document.getElementById('app');
+  app.classList.remove('loading');
+  preact.render(html`<${CSSContextProvider}><${App} /><//>`, app);
 });
