d1e61a9e98dd84ab5763238e07ff72440b81a204
Pablo Molina <contact@pablomolina.me>
2020-09-03 08:03:43 +0200

    Fix category title visibility

--- src/components/BookmarkCategory.js
+++ src/components/BookmarkCategory.js
@@ -3,25 +3,36 @@ import { useCSS } from '../hooks/useCSS.js';
 
 export const BookmarkCategory = ({ bookmark }) => {
   useCSS(`
+    .BookmarkCategory {
+      overflow: hidden;
+      margin: 1.5rem 0.75rem 0.25rem auto;
+      max-width: 3ch;
+      transition: max-width ease var(--transition-duration);
+      will-change: max-width;
+    }
+
     .BookmarkCategory_title {
       white-space: nowrap;
       overflow: hidden;
       font-size: 1.5rem;
       font-weight: 300;
-      max-width: 1ch;
-      margin: 1.5rem 1.125rem 0.25rem auto;
-      transition: max-width ease var(--transition-duration);
-      will-change: max-width;
+      margin: 0 0 0 auto;
+      text-overflow: clip;
+    }
 
+    .Bookmarks:hover .BookmarkCategory {
+      max-width: 80%;
     }
 
     .Bookmarks:hover .BookmarkCategory_title {
-      max-width: calc(100% - 4rem);
+      text-overflow: ellipsis;
     }
   `);
 
   return html`
-    <h1 class="BookmarkCategory_title">${bookmark.title}</h1>
+    <div class="BookmarkCategory">
+      <h1 class="BookmarkCategory_title">${bookmark.title}</h1>
+    </div>
     <${BookmarkChildren} bookmark=${bookmark} />
   `;
 };
