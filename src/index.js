954792a754f3d32fb0508c5e6f04b4c8de076eb8
Pablo Molina <contact@pablomolina.me>
2020-10-04 19:51:04 +0200

    Preload emojis

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import { I18NService } from './services/I18NService.js';
 import { FetchService } from './services/FetchService.js';
 import { bookmarks } from './scripts/bookmarks.js';
 import { editor } from './scripts/editor.js';
+import { EmojiService } from './services/EmojiService.js';
 
 document.head.querySelector('title').innerText = I18NService.get('newTab');
 
-Promise.all([StorageService.init(), FetchService.init()]).then(() => {
+Promise.all([
+  StorageService.init(),
+  FetchService.init(),
+  EmojiService.init(),
+]).then(() => {
   const app = document.getElementById('app');
   app.classList.remove('loading');
 
--- src/services/EmojiService.js
+++ src/services/EmojiService.js
@@ -1,19 +1,19 @@
-let emojis = null;
+let emojis = [];
 
 export const EmojiService = {
-  async getFromText(text) {
-    if (!emojis) {
-      emojis = await fetch('/dependencies/emojilib-2.4.0.json')
-        .then((response) => response.json())
-        .then((json) =>
-          Object.entries(json).map(([key, value]) => ({
-            key,
-            char: value.char,
-            keywords: value.keywords,
-          }))
-        );
-    }
+  async init() {
+    emojis = await fetch('/dependencies/emojilib-2.4.0.json')
+      .then((response) => response.json())
+      .then((json) =>
+        Object.entries(json).map(([key, value]) => ({
+          key,
+          char: value.char,
+          keywords: value.keywords,
+        }))
+      );
+  },
 
+  async getFromText(text) {
     const emoji =
       emojis.find((emoji) => emoji.key === text) ||
       emojis.find((emoji) => emoji.keywords.includes(text)) ||
