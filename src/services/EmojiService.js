46bf2c7b5c29198699339d39a34aafb08a2571cd
Pablo Molina <contact@pablomolina.me>
2021-03-22 18:20:16 +0100

    Load emoji list in background

--- src/services/EmojiService.js
+++ src/services/EmojiService.js
@@ -1,19 +1,21 @@
-let emojis = [];
+let emojisPromise = Promise.resolve([]);
 
 export const EmojiService = {
   async init() {
-    emojis = await fetch('/dependencies/emojilib-2.4.0.json')
+    emojisPromise = fetch('/dependencies/emojilib-2.4.0.json')
       .then((response) => response.json())
-      .then((json) =>
-        Object.entries(json).map(([key, value]) => ({
+      .then((json) => {
+        return Object.entries(json).map(([key, value]) => ({
           key,
           char: value.char,
           keywords: value.keywords,
         }))
-      );
+      });
   },
 
   async getFromText(text) {
+    const emojis = await emojisPromise;
+
     const emoji =
       emojis.find((emoji) => emoji.key === text) ||
       emojis.find((emoji) => emoji.keywords.includes(text)) ||
